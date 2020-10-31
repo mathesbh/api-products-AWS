5f3e50486f338ea65e3294139898c812e42935c4
mathesbh <mathesbh@gmail.com>
2020-10-31 13:58:02 -0300

    Creating get method

--- routes/products.js
+++ routes/products.js
@@ -3,6 +3,14 @@ const router = express.Router();
 
 const Product = require('../models/Product');
 
+router.get('/', (req, res) =>{
+    Product.find().then( products =>{
+        res.status(200).json(products)
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+});
+
 router.post('/', (req, res) =>{
     const newProduct = new Product(req.body);
 
